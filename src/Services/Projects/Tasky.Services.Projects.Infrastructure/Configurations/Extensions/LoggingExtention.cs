0e21733cf6403971ad846656a973200737ab114e
mohammed khairelseed <mkhairelseed@outlook.com>
2026-07-28 12:48:10 +0300

    feat: Add LoggingExtension for Serilog integration and request logging

---
+++ src/Services/Projects/Tasky.Services.Projects.Infrastructure/Configurations/Extensions/LoggingExtention.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Serilog;
+
+namespace Tasky.Services.Projects.Infrastructure.Configurations.Extensions;
+
+public static class LoggingExtension
+{
+    extension(WebApplicationBuilder builder)
+{
+    public void AddLogging() => builder.Host.UseSerilog((hostingContext, configuration) =>
+    {
+        configuration.ReadFrom.Configuration(hostingContext.Configuration);
+    });
+}
+
+extension(WebApplication app)
+{
+    public IApplicationBuilder UseLogging()
+    {
+        app.UseSerilogRequestLogging();
+        return app;
+    }
+}
+}
